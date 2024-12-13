4d40a4fa981b651510106a4af8b264c3c5d9912e
Joe Conlin <{ID}+{username}@users.noreply.github.com>
2024-12-12 20:53:32 -0500

    fix another bug

--- src/pages/RankingForm.js
+++ src/pages/RankingForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import { API_URL } from '../config';
